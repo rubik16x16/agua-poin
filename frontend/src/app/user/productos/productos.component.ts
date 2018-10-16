8efcbd0d9ededb70862ece20d574d48c1057f528
rubik16x16 <drt_mike@hotmail.com>
2018-10-16 15:30:28 -0400

    Seccion de usuario de productos

---
+++ frontend/src/app/user/productos/productos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductosService } from '../../services/productos.service';
+import { Producto } from '../../models/producto';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.scss']
+})
+export class ProductosComponent implements OnInit {
+
+  private productos: Array<Producto>= [];
+
+  constructor(
+    private productosService: ProductosService
+  ) { }
+
+  ngOnInit() {
+    this.getProductos();
+  }
+
+  private getProductos(){
+
+    this.productosService.getProductos().subscribe(
+      productos => {
+        for(let producto of productos){
+          this.productos.push(new Producto(producto.id, producto.nombre, producto.precio, producto.img));
+        }
+      }
+    );
+  }//end getProductos
+}//end ProductosComponent class
--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { NotasComponent } from './notas/notas.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UserRoutingModule
   ],
-  declarations: [UserComponent, NotasComponent]
+  declarations: [UserComponent, NotasComponent, ProductosComponent]
 })
 export class UserModule { }
